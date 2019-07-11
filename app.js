3928cb5f7a19e0a53db94f46a23783d41a0591be
ollie <soulshaymin16@gmail.com>
2019-07-10 19:55:11 -0700

    simple hello world app

---
+++ app.js
@@ -0,0 +1,17 @@
+//reference needed elements 
+const result = document.getElementById('result');
+const nameInput = document.getElementById('name');
+
+//action to take on user event 
+function updateResult() {
+    result.textContent = nameInput.value;
+}
+
+
+const x = 120;
+
+if(x > 20){
+    console.log('hello console', x);
+}
+
+
