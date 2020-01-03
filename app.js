5d2c7f38a4c140be218ba5ee40e7b77d2dca2174
olliecomet <leyechi@gmail.com>
2020-01-02 20:34:14 -0800

    fixed tabbing

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-//reference needed elements 
+// reference needed elements 
 const result = document.getElementById('result');
 const nameInput = document.getElementById('name');
 
-//action to take on user event 
+// action to take on user event 
 function updateResult() {
-    result.textContent = nameInput.value;
+  result.textContent = nameInput.value;
 }
 
-
+// add message to console while var x is greater than 20
 const x = 120;
 
 if(x > 20){
-    console.log('hello console', x);
+  console.log('hello console viewer. this is from when I first began learning JavaScript :>', x);
 }
-
-
